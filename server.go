dcbe692a39449e9e46b8af6d14511d5f28a0f539
Yusuke Takahashi <yusuke_takahashi@cyberagent.co.jp>
2016-10-06 14:13:01 +0900

    Apply logger and recover

--- server.go
+++ server.go
@@ -5,10 +5,15 @@ import (
 
 	"github.com/labstack/echo"
 	"github.com/labstack/echo/engine/standard"
+	"github.com/labstack/echo/middleware"
 )
 
 func main() {
 	e := echo.New()
+
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hello World!")
 	})
