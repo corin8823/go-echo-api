2d537f8aafe23751628569bc380edf59a45d28b0
Yusuke Takahashi <yusuke_takahashi@cyberagent.co.jp>
2016-12-16 16:07:43 +0900

    Add middleware.CORS

--- server.go
+++ server.go
@@ -14,6 +14,7 @@ func main() {
 	e.SetDebug(true)
 	e.Use(middleware.Logger())
 	e.Use(middleware.Recover())
+	e.Use(middleware.CORS())
 
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hello World!")
